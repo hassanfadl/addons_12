37f5083a33e6a20f7b437cf54427abf6fc66fbad
diego gajardo <diegobgajardo@gmail.com>
2020-06-19 01:19:01 -0400

    actualizacion

---
+++ partner/script_oca.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+echo -e "clonando modulos oca"
+
+cd /opt/odoo/server/
+
+mkdir "OCA-addons"
+
+cd  /opt/odoo/server/oca-custom/
+
+git clone -b 12.0 https://github.com/OCA/rest-framework.git
+
+git clone -b 12.0 https://github.com/OCA/account-closing.git
+
+git clone -b 12.0 https://github.com/OCA/manufacture-reporting.git
+
+git clone -b 12.0 https://github.com/OCA/product-pack.git
+
+git clone -b 12.0 https://github.com/OCA/search-engine.git
+
+git clone -b 12.0 https://github.com/OCA/website.git
+
+git clone -b 12.0 https://github.com/OCA/reporting-engine.git
+
+git clone -b 12.0https://github.com/OCA/crm.git
+
+git clone -b 12.0https://github.com/OCA/operating-unit.git
+
+git clone -b 12.0  https://github.com/OCA/server-backend.git
+
+git clone -b 12.0 https://github.com/OCA/stock-logistics-workflow.git
+
+git clone -b 12.0 https://github.com/OCA/geospatial.git
+
+git clone -b 12.0 https://github.com/OCA/project.git
+
+git clone -b 12.0https://github.com/OCA/wms.git
+
+git clone -b 12.0https://github.com/OCA/margin-analysis.git
+
+git clone -b 12.0 https://github.com/OCA/l10n-chile.git
+
+git clone -b 12.0 https://github.com/OCA/maintenance.git
+
+git clone -b 12.0 https://github.com/OCA/fleet.git
+
+git clone -b 12.0 https://github.com/OCA/contract.git
+
+
+git clone -b 12.0 https://github.com/OCA/business-requirement.git
+
+git clone -b 12.0 https://github.com/OCA/connector-telephony.git
+
+
+git clone -b 12.0 https://github.com/OCA/product-variant.git
+
+git clone -b 12.0 https://github.com/OCA/account-payment.git
+
+git clone -b 12.0 https://github.com/OCA/account-invoicing.git
+
+git clone -b 12.0 https://github.com/OCA/account-analytic.git
+
+git clone -b 12.0 https://github.com/OCA/stock-logistics-warehouse.git
+
+git clone -b 12.0 https://github.com/OCA/account-financial-tools.git
+
+git clone -b 12.0 https://github.com/OCA/bank-payment.git
+
+git clone -b 12.0 https://github.com/OCA/OpenUpgrade.git
+
+
+git clone -b 12.0 https://github.com/OCA/helpdesk.git
+
+git clone -b 12.0 https://github.com/OCA/sale-workflow.git
+
+
+git clone -b 12.0 https://github.com/OCA/partner-contact.git
+
+git clone -b 12.0 https://github.com/OCA/product-attribute.git
+
+git clone -b 12.0 https://github.com/OCA/purchase-workflow.git
+
+git clone -b 12.0 https://github.com/OCA/management-system.git
+
+git clone -b 12.0 https://github.com/OCA/manufacture.git
+
+git clone -b 12.0 https://github.com/OCA/maintainer-quality-tools.git
+
+git clone -b 12.0 https://github.com/OCA/odoorpc.git
+
+git clone -b 12.0 https://github.com/OCA/oca-custom.git
+
+git clone -b 12.0 https://github.com/OCA/sale-reporting.git
+
+git clone -b 12.0 https://github.com/OCA/account-consolidation.git
+
+git clone -b 12.0 https://github.com/OCA/account-invoice-reporting.git
+
+git clone -b 12.0 https://github.com/OCA/data-protection.git
+
+git clone -b 12.0 https://github.com/OCA/interface-github.git
+
+git clone -b 12.0 https://github.com/OCA/purchase-reporting.git
+
+git clone -b 12.0 https://github.com/OCA/stock-logistics-transport.git
+
+git clone -b 12.0 https://github.com/OCA/connector-interfaces.git
+
+git clone -b 12.0 https://github.com/OCA/server-brand.git
+
+git clone -b 12.0 https://github.com/OCA/mis-builder.git
+
+git clone -b 12.0 https://github.com/OCA/stock-logistics-reporting.git
+
+git clone -b 12.0 https://github.com/OCA/project-reporting.git
+
+git clone -b 12.0 https://github.com/OCA/server-env.git
+
+
+git clone -b 12.0 https://gitlab.com/dansanti/facturacion_electronica.git
+
+git clone -b 12.0 https://gitlab.com/dansanti/l10n_cl_fe.git
+
+
+git clone -b 12.0 https://github.com/diegobgl/addons_12.git
+
+git clone -b 12.0 https://github.com/KonosCL/addons-konos.git
